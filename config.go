a56ac7cedf68ceaf0f648d96524404e52df15396
Daniel Leech <dan.t.leech@gmail.com>
2021-05-30 16:16:47 +0100

    Support loading config

--- config.go
+++ config.go
@@ -5,52 +5,64 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+
 	"gopkg.in/yaml.v2"
 )
 
 type Action struct {
-    Type string `yaml:"type"`;
-    Params interface{} `yaml:"params"`;
+	Type   string      `yaml:"type"`
+	Params interface{} `yaml:"params"`
 }
 
 type Rule struct {
-    Predicate string `yaml:"rule"`;
-    Actions []Action `yaml:"actions"`;
+	Predicate string   `yaml:"rule"`
+	Actions   []Action `yaml:"actions"`
 }
 
 type Config struct {
-    Address string `yaml:"address"`;
-    WorkspacePath string `yaml:"workspacePath"`;
-    Rules []Rule `yaml:"rules"`;
+	Address       string `yaml:"address"`
+	WorkspacePath string `yaml:"workspacePath"`
+	Rules         []Rule `yaml:"rules"`
 }
 
 func LoadConfig(path string) Config {
-    if path == "" {
-        return Config{
-        	Address:       ":8080",
-        	WorkspacePath: "workspace",
-        	Rules:         []Rule{},
-        }
-    }
+	if path != "" {
+		return loadConfig(path)
+	}
 
-    if _, err := os.Stat(path); os.IsNotExist(err) {
-        log.Fatal(fmt.Sprintf("Config file not found at: %s", path))
-    }
+	for _, path := range [2]string{"artag.yml", "artag.yml.dist"} {
+		if _, err := os.Stat(path); !os.IsNotExist(err) {
+			return loadConfig(path)
+		}
+	}
 
-    rawConfig, err := ioutil.ReadFile(path)
-
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    config := Config{
-    }
-    e := yaml.Unmarshal([]byte(rawConfig), &config)
-    if e != nil {
-        log.Fatal(e.Error())
-    }
-
-    return config
+	return loadConfig("")
 }
 
+func loadConfig(path string) Config {
+	if path == "" {
+		return Config{
+			Address:       ":8080",
+			WorkspacePath: "workspace",
+			Rules:         []Rule{},
+		}
+	}
 
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		log.Fatal(fmt.Sprintf("Config file not found at: %s", path))
+	}
+
+	rawConfig, err := ioutil.ReadFile(path)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	config := Config{}
+	e := yaml.Unmarshal([]byte(rawConfig), &config)
+	if e != nil {
+		log.Fatal(e.Error())
+	}
+
+	return config
+}
--- config_test.go
+++ config_test.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"os"
 	"testing"
 
-	"github.com/davecgh/go-spew/spew"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestLoadConfig(t *testing.T) {
-    config := LoadConfig("testdata/config/example.yml")
-    assert.Equal(t, "127.0.0.1:9999", config.Address)
-    spew.Dump(config)
+	config := LoadConfig("testdata/config/example.yml")
+	assert.Equal(t, "127.0.0.1:9999", config.Address)
+}
+
+func TestLoadDefaultConfig(t *testing.T) {
+	wd, _ := os.Getwd()
+	os.Chdir("testdata/config")
+	config := LoadConfig("")
+	assert.Equal(t, "127.0.0.1:7777", config.Address)
+	os.Chdir(wd)
 }
